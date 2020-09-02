c4324a5ad9c117a1ea500c7b44fad63100e1acd7
Daniel Marques Fonseca <daniel_mf@live.com>
2020-09-02 10:14:56 -0300

    Foreign key not null (referencial integrity)

--- sales_web_mvc/Migrations/20200902131000_DepartmentForeignKey.Designer.cs
+++ sales_web_mvc/Migrations/20200902131000_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sales_web_mvc.Data;
 
 namespace sales_web_mvc.Migrations
 {
     [DbContext(typeof(sales_web_mvcContext))]
-    partial class sales_web_mvcContextModelSnapshot : ModelSnapshot
+    [Migration("20200902131000_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
