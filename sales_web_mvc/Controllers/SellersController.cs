273d36eb3e955f998f8be476b803e6f2475707ab
Daniel Marques Fonseca <daniel_mf@live.com>
2020-09-02 10:04:52 -0300

    Simple Create form

--- sales_web_mvc/Controllers/SellersController.cs
+++ sales_web_mvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using sales_web_mvc.Models;
 using sales_web_mvc.Services;
 
 namespace sales_web_mvc.Controllers {
@@ -17,6 +18,17 @@ namespace sales_web_mvc.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
